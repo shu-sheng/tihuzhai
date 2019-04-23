4bebf4ef1c1238ce97f7a9939d0a63b1798e2013
shusheng <174185499@qq.com>
2019-04-23 18:42:44 +0800

    SpringBoot集成模板引擎thymeleaf

---
+++ tihuzhai-web/src/main/java/com/shusheng/tihuzhai/web/controller/TestThymeleafController.java
@@ -0,0 +1,24 @@
+package com.shusheng.tihuzhai.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author shusheng
+ * @description
+ * @Email eric_wu_peng@126.com
+ * @date 2019/4/23 18:39
+ */
+@Controller
+@RequestMapping("/tpl")
+public class TestThymeleafController {
+    @RequestMapping("/testThymeleaf")
+    public String testThymeleaf(ModelMap map) {
+        // 设置属性
+        map.addAttribute("name", "enjoy");
+        // testThymeleaf：为模板文件的名称
+        // 对应src/main/resources/templates/testThymeleaf.html
+        return "testThymeleaf";
+    }
+}
