5692c1b2496932038200562dc8cc4ba458c33c32
shusheng <174185499@qq.com>
2019-04-23 19:03:20 +0800

    SpringBoot集成Swagger2构建API文档

---
+++ tihuzhai-web/src/main/java/com/shusheng/tihuzhai/web/controller/swagger/TestSwaggerController.java
@@ -0,0 +1,33 @@
+package com.shusheng.tihuzhai.web.controller.swagger;
+
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author shusheng
+ * @description
+ * @Email eric_wu_peng@126.com
+ * @date 2019/4/23 18:51
+ */
+@RestController
+@RequestMapping(value="/swagger")
+public class TestSwaggerController {
+
+    @ApiOperation(value="获取用户信息", notes="根据id来获取用户详细信息")
+    @ApiImplicitParam(name="id", value="用户ID", required=true, dataType="String")
+    @RequestMapping(value="/{id}", method= RequestMethod.GET)
+    public Map<String,String> getInfo(@PathVariable String id) {
+        Map<String ,String> map = new HashMap<String, String>();
+        map.put("name", "lison");
+        map.put("age", "38");
+        return map;
+    }
+
+}
