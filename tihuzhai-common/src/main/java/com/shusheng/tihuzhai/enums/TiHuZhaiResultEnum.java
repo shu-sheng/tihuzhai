adcd3b37738402c794e8e12f72287599388bd85a
shusheng <174185499@qq.com>
2019-05-13 17:48:35 +0800

    醍醐斋接口返回结果枚举

--- tihuzhai-common/src/main/java/com/shusheng/tihuzhai/enums/TiHuZhaiResultEnum.java
+++ tihuzhai-common/src/main/java/com/shusheng/tihuzhai/enums/TiHuZhaiResultEnum.java
@@ -6,8 +6,8 @@ import java.util.List;
 /**
  * @author shusheng
  * @description
- * @Email shusheng@yiji.com
- * @date 2018/12/26 19:03
+ * @Email eric_wu_peng@126.com
+ * @date 2019/4/24 17:51
  */
 public enum TiHuZhaiResultEnum {
 
@@ -21,15 +21,28 @@ public enum TiHuZhaiResultEnum {
     HAVE_NOT_DATA("HAVE_NOT_DATA", "没有数据"),
     /** 该用户对该数据无访问权限 */
     NO_ACCESS("NO_ACCESS", "该用户对该数据无访问权限"),
-
+    /** 签名不正确 */
+    ILLEGAL_SIGN("ILLEGAL_SIGN", "签名不正确"),
+    /** 冻结异常 */
+    FREEZE_ERROR("FREEZE_ERROR", "冻结异常"),
+    /** 未实名认证 */
+    NO_REAL_NAME("NO_REAL_NAME", "未实名认证"),
     /** 密码错误 */
     PASSWORD_ERROR("PASSWORD_ERROR", "密码错误"),
+    /** 用户冻结 */
+    USER_DISABLE("USER_DISABLE", "用户冻结"),
     /** 用户锁定 */
     USER_LOCKED("USER_LOCKED", "用户锁定"),
+    /** 功能未开通 */
+    FUNCTION_NOT_OPEN("FUNCTION_NOT_OPEN", "功能未开通"),
+    /** 执行成功 */
+    EXECUTE_SUCCESS("EXECUTE_SUCCESS", "执行成功"),
     /** 用户已经存在 */
     USER_EXIST("USER_EXIST", "用户已经存在"),
     /** 不存在该用户 */
-    USER_NOEXIST("USER_EXIST", "不存在该用户");
+    USER_NOEXIST("USER_EXIST", "不存在该用户"),
+    /** 执行失败 */
+    EXECUTE_FAILURE("EXECUTE_FAILURE", "执行失败");
 
     /** 枚举值 */
     private final String code;
@@ -38,7 +51,7 @@ public enum TiHuZhaiResultEnum {
     private final String message;
 
     /**
-     * 构造一个<code>MandarinResultEnum</code>枚举对象
+     * 构造一个<code>TiHuZhaiResultEnum</code>枚举对象
      *
      * @param code
      * @param message
@@ -80,7 +93,7 @@ public enum TiHuZhaiResultEnum {
      * 通过枚举<code>code</code>获得枚举
      *
      * @param code
-     * @return MandarinResultEnum
+     * @return TiHuZhaiResultEnum
      */
     public static TiHuZhaiResultEnum getByCode(String code) {
         for (TiHuZhaiResultEnum _enum : values()) {
@@ -94,7 +107,7 @@ public enum TiHuZhaiResultEnum {
     /**
      * 获取全部枚举
      *
-     * @return List<MandarinResultEnum>
+     * @return List<TiHuZhaiResultEnum>
      */
     public List<TiHuZhaiResultEnum> getAllEnum() {
         List<TiHuZhaiResultEnum> list = new ArrayList<TiHuZhaiResultEnum>();
