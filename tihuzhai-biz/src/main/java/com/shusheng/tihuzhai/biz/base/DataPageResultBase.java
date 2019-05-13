da3148c41d55dc63c361dc8c56e86cde421cc56a
shusheng <174185499@qq.com>
2019-05-14 01:12:57 +0800

    返回结果增加数据泛型基类

--- tihuzhai-biz/src/main/java/com/shusheng/tihuzhai/biz/base/DataPageResultBase.java
+++ tihuzhai-biz/src/main/java/com/shusheng/tihuzhai/biz/base/DataPageResultBase.java
@@ -1,5 +1,6 @@
-package com.shusheng.tihuzhai.biz.hiboss.base;
+package com.shusheng.tihuzhai.biz.base;
 
+import com.shusheng.tihuzhai.biz.base.TiHuZhaiPageResultBase;
 import com.shusheng.tihuzhai.biz.base.TiHuZhaiResultBase;
 
 import java.util.List;
@@ -10,7 +11,7 @@ import java.util.List;
  * @Email eric_wu_peng@126.com
  * @date 2019/4/30 15:01
  */
-public class DataResultBase<T> extends TiHuZhaiResultBase {
+public class DataPageResultBase<T> extends TiHuZhaiPageResultBase {
     private static final long serialVersionUID = 707248451525534779L;
 
     private	T data;
---
+++ tihuzhai-biz/src/main/java/com/shusheng/tihuzhai/biz/base/DataResultBase.java
@@ -0,0 +1,35 @@
+package com.shusheng.tihuzhai.biz.base;
+
+import com.shusheng.tihuzhai.biz.base.TiHuZhaiResultBase;
+
+import java.util.List;
+
+/**
+ * @author shusheng
+ * @description
+ * @Email eric_wu_peng@126.com
+ * @date 2019/4/30 15:01
+ */
+public class DataResultBase<T> extends TiHuZhaiResultBase {
+    private static final long serialVersionUID = 707248451525534779L;
+
+    private	T data;
+
+    private List<T> datas;
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public List<T> getDatas() {
+        return datas;
+    }
+
+    public void setDatas(List<T> datas) {
+        this.datas = datas;
+    }
+}
